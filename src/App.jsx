8cc3a954068ecca2cbed5a3289ebe8f2f35e6419
num4yo3 <82578883+num4yo3@users.noreply.github.com>
2021-12-20 14:59:29 +0900

    リストをjsで定義した

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,50 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  const [incomp, setIncomp] = useState([
+    "ペットの餌やり",
+    "弁当作り",
+    "郵便物の投函"
+  ]);
+  const [comp, setComp] = useState(["腹筋50回"]);
+  return (
+    <>
+      <h1 className="title">TODOリスト</h1>
+      <input placeholder="ToDoリストを入力"></input>
+      <button>登録</button>
+      <div className="todo_task">
+        <h2 className="h2title">未完了リスト</h2>
+        <ul>
+          {incomp.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="todo_list">
+                  <p>{todo}</p>
+                  <button className="btn01">完了</button>
+                  <button className="btn02">削除</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="completed_task">
+        <h2 className="h2title">完了済リスト</h2>
+        <ul>
+          {comp.map((todo) => {
+            return (
+              <li key={todo}>
+                <div className="todo_list">
+                  <p>腹筋50回</p>
+                  <button className="btn01">戻す</button>
+                  <button className="btn02">削除</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import { App } from "./App";
+import { App } from "./App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
